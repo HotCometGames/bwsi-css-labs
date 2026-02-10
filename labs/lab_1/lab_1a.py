7379c4e4f080bde88ff63df81058a61a04bb731c
Caleb Gleason <hotcometgames@gmail.com>
2026-02-09 20:52:40 -0600

    Added intro to lab_1a

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Caleb Gleason" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi, my name is Caleb and Im a junior from Illinois. I enjoy game dev, playing and listening to music (Im a percussionist), and I love all kind of sports. Im excited to meet and know all of you, and to learn the material of this program!")
 
 if __name__ == "__main__":
     main()
